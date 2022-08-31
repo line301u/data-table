41f1ee24dbcf4b3ac9808da1f62c3bd6662b2bc2
linealind <linea.lindebjerg@hotmail.com>
2022-08-31 14:33:53 +0200

    final push

--- components/DataFilter.vue
+++ components/DataFilter.vue
@@ -1,63 +1,66 @@
 <template v-slot:body.append>
-  <v-container class="d-flex">
-    <v-col>
-      <template>
-        <v-text-field
-          v-model="selectedFilterValues.search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-          @keyup="setFilter(selectedFilterValues.search, 'search')"
-        ></v-text-field>
-      </template>
-    </v-col>
-    <v-col>
-      <v-select
-        multiple
-        label="Filter by gender"
-        :items="gender_list"
-        v-model="selectedFilterValues.gender"
-        @change="setFilter(selectedFilterValues.gender, 'gender')"
-        >
-        <template v-slot:selection="{ item, index }">
-          <v-chip v-if="index === 0">
-            <span>{{ item }}</span>
-          </v-chip>
-          <span
-            v-if="index === 1"
-            class="grey--text text-caption"
-          >
-            (+{{ selectedFilterValues.gender.length - 1 }} others)
-          </span>
-      </template>
-      </v-select>
-    </v-col>
-    
-    <v-col>
-      <v-select
-        multiple
-        label="Filter by country"
-        :items="country_list"
-        v-model="selectedFilterValues.country"
-        @change="setFilter(selectedFilterValues.country, 'country')"
-        >
-        <template v-slot:selection="{ item, index }">
-          <v-chip v-if="index === 0">
-            <span>{{ item }}</span>
-          </v-chip>
-          <span
-            v-if="index === 1"
-            class="grey--text text-caption"
-          >
-            (+{{ selectedFilterValues.country.length - 1 }} others)
-          </span>
-        </template>
-      </v-select>
-    </v-col>
-
-    <button @click="clearFilter(); clearFilterValues();">Clear filter</button>
-  </v-container>
+  <div class="filter-container">
+      <div class="filter d-flex">
+        <div class="filter__search">
+          <template>
+            <v-text-field
+              v-model="selectedFilterValues.search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+              @keyup="setFilter(selectedFilterValues.search, 'search')"
+            ></v-text-field>
+          </template>
+        </div>
+        <div class="filter__select d-flex">
+          <v-select
+            multiple
+            label="Filter by gender"
+            :items="gender_list"
+            v-model="selectedFilterValues.gender"
+            color="unset"
+            @change="setFilter(selectedFilterValues.gender, 'gender')"
+            >
+            <template v-slot:selection="{ item, index }">
+              <v-chip v-if="index === 0">
+                <span>{{ item }}</span>
+              </v-chip>
+              <span
+                v-if="index === 1"
+                class="grey--text text-caption"
+              >
+                (+{{ selectedFilterValues.gender.length - 1 }} others)
+              </span>
+          </template>
+          </v-select>
+        
+          <v-select
+            multiple
+            label="Filter by country"
+            :items="country_list"
+            v-model="selectedFilterValues.country"
+            color="unset"
+            @change="setFilter(selectedFilterValues.country, 'country')"
+            >
+            <template v-slot:selection="{ item, index }">
+              <v-chip v-if="index === 0">
+                <span>{{ item }}</span>
+              </v-chip>
+              <span
+                v-if="index === 1"
+                class="grey--text text-caption"
+              >
+                (+{{ selectedFilterValues.country.length - 1 }} others)
+              </span>
+            </template>
+          </v-select>
+          <div class="button-wrapper">
+            <button class="button" @click="clearFilter(); clearFilterValues();">Clear filter</button>
+          </div>
+        </div>
+      </div>
+  </div>
 </template>
 
 
@@ -83,22 +86,144 @@ export default {
       this.selectedFilterValues.country = "";
       this.selectedFilterValues.search = "";
     }
-  }
+  },
 }
 </script>
 
-<style lang="sass" scoped>
-  .v-data-table
-    max-width: 100%
-</style>
-
 <style lang="sass">
-  .v-select
-    &__selections
-      min-height: 40px
+  @import "../assets/sass/_variables.sass"
+  
+  .filter-container
+    margin-top: 1.5rem
 
-  .v-input
-    &__control
-      min-height: 40px
+    .filter
+      flex-direction: column
+      justify-content: flex-start
+
+      @media screen and (min-width: 775px) 
+        flex-direction: row
+        justify-content: space-between
+        margin-bottom: 2rem
+
+      &__select
+        flex-shrink: 1
+        flex-direction: column
+
+        @media screen and (min-width: 775px) 
+          flex-direction: row
+          justify-content: space-between
+          justify-content: flex-start
+
+      &__search
+        flex-grow: 1
+        width: 100%
+        
+        @media screen and (min-width: 775px) 
+          max-width: 350px
+
+        .v-input
+          &__slot
+            border: 1px solid $rs__grey-2
+            border-radius: 0.3rem
+          
+          &__control
+            @media screen and (min-width: 775px) 
+              margin-right: 1rem
+        
+          .v-label
+            top: 6px
+
+          .primary--text
+            color: $rs__primary-7 !important
+
+        .v-text-field input
+          color: black
+          font-size: 0.825rem
+          padding-left: .5rem
+
+      .v-select
+        &__selections
+          min-height: 40px
+
+        &__slot
+          width: 100%
+          background-color: $rs__grey-1
+          border-radius: 0.3rem
+
+          @media screen and (min-width: 775px) 
+            margin-right: 1rem
+            width: 220px
+
+          &::before
+            border-color: transparent !important
+            border-style: none
+
+      .v-chip
+        background: $rs__grey-2 !important
+
+    .v-text-field
+      .v-label
+        top: 10px
+        font-size: 0.825rem
+        color: $rs__grey-6 !important
+        margin-left: .5rem
+      
+      .v-label--active
+        transform: translateY(-28px) scale(0.75)
+        color: red
+        margin-left: 0rem
+        color: $rs__grey-6 !important
+
+      .v-input__control > .v-input__slot:before
+        border-color: transparent !important
+        border-style: none
+
+    .v-input
+      &__control
+        min-height: 40px
+      
+      &__slot
+        margin-bottom: .4rem !important
+
+      &__icon
+        i
+          color: $rs__grey-4 !important
+
+      &__append-inner
+        margin-top: 7px !important
+        margin-right: 5px
+
+    .button-wrapper
+      display: flex
+      margin-bottom: 3rem
+      margin-top: 1rem
+      justify-content: flex-end
+
+      @media screen and (min-width: 775px) 
+        margin-bottom: 0rem
+        margin-top: 8px
+        align-self: center
+        align-items: center
+        justify-content: flex-start
+
+
+    .button 
+      font-size: 0.825rem
+      color: $rs__grey-6
+      min-width: 100px
+      background-color: $rs__grey-2
+      padding: .2rem 1rem
+      border-radius: 0.3rem
+      max-height: 30px
+  
+  .v-messages, .v-text-field__details
+    display: none !important
+
+  div.filter__select.d-flex > div.v-input.v-input--is-focused.theme--light.v-text-field.v-text-field--is-booted.v-select.v-select--is-menu-active.v-select--is-multi.primary--text > div > div.v-input__slot > div.v-select__slot > div.v-input__append-inner > div > i
+    color: $rs__primary-8 !important
+
+  div.filter__select.d-flex > div.v-input.v-input--is-focused.theme--light.v-text-field.v-text-field--is-booted.v-select.v-select--is-multi.primary--text > div > div.v-input__slot > div.v-select__slot > div.v-input__append-inner > div > i
+    color: $rs__grey-4 !important
+
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
           :server-items-length="totalItems"
           :loading="loading"
           :footer-props="{'items-per-page-options':[5, 10, 15, 30, 50]}"
-        ).elevation-1.mt-10
+        ).elevation-1.mt-2
 
 </template>
 
@@ -46,24 +46,10 @@ export default {
         { text: 'Firstname', value: "user.first_name", align: 'start'},
         { text: 'Lastname', value: 'user.last_name'},
         { text: 'Email', value: 'email' },
-        { text: 'Gender', value: 'gender', 
-            filter: value => {
-              const selectedValues = this.filterSettings.gender;
-
-              if (selectedValues.length <= 0) return true
-              if (selectedValues.includes(value)) return value
-            },
-        },
+        { text: 'Gender', value: 'gender'},
         { text: 'Year', value: 'year' },
         { text: 'Sales', value: 'sales' },
-        { text: 'Country', value: 'country',
-            filter: value => {
-              const selectedValues = this.filterSettings.country;
-
-              if (selectedValues.length <= 0) return true
-              if (selectedValues.includes(value)) return value
-            },
-        },
+        { text: 'Country', value: 'country'},
       ],
     }
   },
@@ -150,8 +136,6 @@ export default {
             items = items.slice((page - 1) * itemsPerPage, page * itemsPerPage)
           }
 
-          console.log(items)
-
           setTimeout(() => {
             resolve({
               items,
@@ -160,11 +144,10 @@ export default {
           }, 2000)
         })
     },
-  
     setFilter(selectedValues, filterType) {
-      console.log(filterType)
       if (filterType === "gender"){
         this.filterSettings.gender = selectedValues;
+        
         this.getDataFromApi();
       }
 
@@ -191,9 +174,128 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-.v-progress-circular
-  position: absolute
-  top: 50%
-  left: 50%
+<style lang="sass">
+  @import "../assets/sass/_variables.sass"
+
+  .v-main__wrap
+    margin-bottom: 5rem
+
+  .v-application .elevation-1
+    box-shadow: none !important
+    box-shadow: 2.4px 0px 3.6px rgba(0, 0, 0, 0.009), 6.5px 0px 10px rgba(0, 0, 0, 0.05), 15.7px 0px 24.1px rgba(0, 0, 0, 0.013), 52px 0px 80px rgba(0, 0, 0, 0.04) !important
+    border: 1px solid $rs__grey-1
+
+  .v-data-table-header
+    tr
+      background: $rs__grey-2
+      height: 32px !important
+
+    span
+      color: $rs__grey-6
+      text-transform: uppercase
+    
+    th
+      padding-top: 2px !important
+
+    .v-icon
+      font-size: 1rem !important
+      margin-left: .2rem
+
+      &::before
+        margin-bottom: 3px
+        color: $rs__primary-7   
+
+  .theme--light.v-data-table > .v-data-table__wrapper > table > tbody > tr:not(:last-child) > td:not(.v-data-table__mobile-row)
+    border-bottom: 1px solid $rs__grey-2
+
+  .theme--light.v-data-table > .v-data-table__wrapper > table > thead > tr:last-child > th
+    border-bottom: 1px solid $rs__grey-2
+
+  .v-text-field.v-input--is-focused > .v-input__control > .v-input__slot:after
+    transform: scale(0)
+
+  tbody
+    tr
+      &:nth-child(2n)
+        background: $rs__grey-1
+        transition: .3s
+      
+      &:hover 
+        background-color: $rs__grey-2 !important
+        transition: .3s
+        
+    td
+      height: 42px !important
+
+
+  .v-progress-linear__buffer
+    background-color: $rs__grey-3 !important
+
+  .v-list-item
+    &__title
+      font-size: 0.895rem
+      padding: 0.2rem 0
+    
+    &__action
+      i.primary--text
+        color: $rs__primary-8 !important 
+    
+    &--active 
+      background-color: white !important
+      
+      .v-list-item__title 
+        color: black !important
+
+  .v-data-table-header
+    .v-select-list
+      width: 237px
+
+    .v-menu
+      &::after
+        display: none
+
+    &__content:first-child
+      top: 170px !important
+
+  .v-data-footer
+    border-top: 1px solid $rs__grey-2 !important
+    padding: .5rem .3rem
+    justify-content: center
+
+    @media screen and (min-width: 775px) 
+      justify-content: end
+
+    .v-input
+      &__control
+        background: $rs__grey-1 !important
+        border-radius: 0.7rem
+        padding: 0 .7rem
+      
+      &__slot::before
+        display: none
+
+    .v-select
+      margin: 13px 0 13px 15px !important
+
+  .v-data-table-header-mobile__wrapper
+    .v-label
+      color: $rs__grey-6
+      text-transform: uppercase
+      font-size: .825rem
+
+    .v-select__slot
+      .primary--text
+        color: $rs__grey-6 !important
+
+    .v-input__slot::before
+      display: none
+    
+    .v-chip
+      background: white !important
+      margin: 4px 0 0 0!important
+
+      &__content
+        text-transform: capitalize
+        font-weight: 400
+
 </style>
